464d5c02b9f0df7f7cf90e84c2e718a20a7867e8
raimennn <54454526+raimennn@users.noreply.github.com>
2024-05-22 02:32:58 -0300

    Update privateBankAudited.sol

--- privateBankAudited.sol
+++ privateBankAudited.sol
@@ -1,1 +1,51 @@
+pragma solidity 0.8.20;
 
+// SPDX-License-Identifier: MIT
+
+contract PrivateBank {
+    mapping(address => uint256) private balances;
+
+//Tiny fix: Adding events for "deposit" and "withdraw" functions.
+    event Deposit(address indexed user, uint256 amount);
+    event Withdrawal(address indexed user, uint256 amount);
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);  // Emit deposit event
+    }
+
+//First major vulnerability spotted in the "withdraw" function.
+//Type: reentrancy vulnerability.
+//Cause: The function can be called multiple times, draining the victim's wallet because of the possibility of withdrawing funds from the target without interruption between each call.
+  This could be done before the victim notices it because the balance updates after sending ETH, leaving time in between that a bot can take advantage of for this reentrancy attack.
+//Modifications: Changed "call" method to "transfer". This is because "transfer" is less flexible, having a gas limit of 2300 which is insufficient to reenter most contracts, preventing potential 
+                 reentrancy attacks that are more common if the gas for the transaction is not limited. Moreover, "transfer" provides automatic error handling, reverting the transaction if a failure occurs.
+                 Finally, I added an event log at the end for better handling of information for each movement.
+
+    function withdraw() external {
+        uint256 balance = getUserBalance(msg.sender);
+
+        require(balance > 0, "Insufficient balance");
+
+        //Updating balance before the funds sending.
+
+        balances[msg.sender] = 0; 
+
+        payable(msg.sender).transfer(balance);
+
+        emit Withdrawal(msg.sender, balance);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+//Second major vulnerability spotted in the "getUserBalance" function.
+//Type: query balances of any user vulnerability.
+//Cause: This happens because the function is marked as "public", which leads to an unwanted display of information to external users unrelated to the balance displayed. This is primarily a privacy concern for anyone.
+//Modifications: Changed the function from "public" to "internal", providing the balance information only to the user related to it and the contract owner.
+
+    function getUserBalance(address _user) public view returns (uint256) {
+        return balances[_user];
+    }
+}
